42a810ec5df71a79f4f2c9a0eff68ab0b711dde2
Hadrien Croubois <hc@iex.ec>
2019-08-26 11:12:04 +0200

    adding portis module

---
+++ module-portis/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+	mode:  "production",
+	entry: "./lib/index.js",
+	output:
+	{
+		path:          path.resolve(__dirname, "public"),
+		filename:      "enslogin-module-portis.js",
+		library:       "enslogin-module-portis",
+		libraryTarget: "umd",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				// include: [ path.resolve(__dirname, "dist") ]
+				// exclude: [ path.resolve(__dirname, "node_modules") ],
+				loader: "babel-loader",
+			},
+		]
+	}
+};
