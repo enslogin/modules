1c428a3338b8c7324bb8759144cfd821668b6886
Miguel Mota <hello@miguelmota.com>
2019-08-28 10:42:16 -0700

    Add .editorconfig and run eclinter

--- module-portis/webpack.config.js
+++ module-portis/webpack.config.js
@@ -1,23 +1,22 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-	mode:  "production",
-	entry: "./lib/index.js",
-	output:
-	{
-		path:          path.resolve(__dirname, "public"),
-		filename:      "enslogin-module-portis.js",
-		library:       "enslogin-module-portis",
-		libraryTarget: "umd",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				// include: [ path.resolve(__dirname, "dist") ]
-				// exclude: [ path.resolve(__dirname, "node_modules") ],
-				loader: "babel-loader",
-			},
-		]
-	}
-};
+  mode: 'production',
+  entry: './lib/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'enslogin-module-portis.js',
+    library: 'enslogin-module-portis',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        // include: [ path.resolve(__dirname, "dist") ]
+        // exclude: [ path.resolve(__dirname, "node_modules") ],
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
