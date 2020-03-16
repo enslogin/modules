0ed395f6eb93089383c7b6a8e4dc6dbb380c74b8
Hadrien Croubois <hc@iex.ec>
2020-03-16 15:06:02 +0100

    unilogin fix

--- modules/unilogin/lib/index.js
+++ modules/unilogin/lib/index.js
@@ -1,13 +1,29 @@
 const UniLogin = require('@unilogin/provider')
+const { providers } = require('ethers')
 
 global.provider = (config) => new Promise((resolve, reject) => {
   const provider = UniLogin.create(config.provider.network);
-  provider.enable = async () => {
-    console.info('[unilogin:enable] no such function');
-    return provider;
-  }
-  provider.disable = async () => {
+
+  provider.enable = () => new Promise((resolve, reject) => {
+    provider.send(
+      { method: 'eth_requestAccounts' },
+      (err, res) => {
+        if(err)
+        {
+          reject(err);
+        }
+        else
+        {
+          resolve(provider);
+        }
+      }
+    )
+  })
+
+  provider.disable = () => new Promise((resolve, reject) => {
     console.info('[unilogin:disable] no such function');
-  }
+    resolve();
+  })
+
   resolve(provider);
 })
