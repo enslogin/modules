fc1aaf72e326f07fba1ac7879f875a21db162c82
Hadrien Croubois <hc@iex.ec>
2020-03-19 22:07:23 +0100

    unilogin now has enable + rebuild

--- modules/unilogin/lib/index.js
+++ modules/unilogin/lib/index.js
@@ -1,29 +1,5 @@
 const UniLogin = require('@unilogin/provider')
-const { providers } = require('ethers')
 
 global.provider = (config) => new Promise((resolve, reject) => {
-  const provider = UniLogin.create(config.provider.network);
-
-  provider.enable = () => new Promise((resolve, reject) => {
-    provider.send(
-      { method: 'eth_requestAccounts' },
-      (err, res) => {
-        if(err)
-        {
-          reject(err);
-        }
-        else
-        {
-          resolve(provider);
-        }
-      }
-    )
-  })
-
-  provider.disable = () => new Promise((resolve, reject) => {
-    console.info('[unilogin:disable] no such function');
-    resolve();
-  })
-
-  resolve(provider);
+  resolve(UniLogin.create(config.provider.network));
 })
