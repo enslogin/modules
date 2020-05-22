3b087131ae80cd8ab50a637ef90dbc253cac3055
Hadrien Croubois <hc@iex.ec>
2020-05-22 11:33:57 +0200

    dep updates, not pushed

--- modules/portis/lib/index.js
+++ modules/portis/lib/index.js
@@ -5,6 +5,7 @@ global.provider = (config) => new Promise((resolve, reject) => {
     config._portis ? config._portis.appId : '3269932e-25a4-4350-b543-e5e762acb9ae',
     config.provider.network
   )
-  portis.provider.disable = () => portis.logout()
+  portis.provider.disable       = portis.logout;
+  portis.provider.changeNetwork = portis.changeNetwork;
   resolve(portis.provider)
 })
