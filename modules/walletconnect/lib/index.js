1dd6618bec1d5af522c5e101a8da6e27e938a0d5
Hadrien Croubois <hc@iex.ec>
2020-06-08 22:21:07 +0200

    wallet connect module

--- modules/walletconnect/lib/index.js
+++ modules/walletconnect/lib/index.js
@@ -4,8 +4,6 @@ global.provider = (config) => new Promise((resolve, reject) => {
   const provider = new WalletConnectProvider({
     infuraId: this.state.config._infura ? this.state.config._infura.key : '27e484dcd9e3efcfd25a83a78777cdf1'
   });
-  provider.disable = async () => {
-    provider.close();
-  }
+  provider.disable = provider.close;
   resolve(provider);
 })
