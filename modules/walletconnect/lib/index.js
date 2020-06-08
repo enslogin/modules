b55b9ed9de927c4d5e6c3cf2a5fa6e7b41954bca
Hadrien Croubois <hc@iex.ec>
2020-06-08 22:28:45 +0200

    wallet connect module fix

--- modules/walletconnect/lib/index.js
+++ modules/walletconnect/lib/index.js
@@ -2,7 +2,7 @@ const WalletConnectProvider = require('@walletconnect/web3-provider')
 
 global.provider = (config) => new Promise((resolve, reject) => {
   const provider = new WalletConnectProvider({
-    infuraId: this.state.config._infura ? this.state.config._infura.key : '27e484dcd9e3efcfd25a83a78777cdf1'
+    infuraId: config._infura ? config._infura.key : '27e484dcd9e3efcfd25a83a78777cdf1'
   });
   provider.disable = provider.close;
   resolve(provider);
