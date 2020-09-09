efec8dda54bff66009be342707dbaa8b9a735858
Hadrien Croubois <hc@iex.ec>
2020-09-09 23:22:28 +0200

    fix walletlink disable

--- modules/walletlink/lib/index.js
+++ modules/walletlink/lib/index.js
@@ -2,14 +2,16 @@ import WalletLink from 'walletlink'
 import ethers from 'ethers'
 
 global.provider = (config) => new Promise((resolve, reject) => {
-	const walletLink = new WalletLink({ appName: 'ENSLogin' })
-	const p          = ethers.getDefaultProvider(config.provider.network)
-	const provider   = (p instanceof ethers.providers.FallbackProvider) ? p.providerConfigs.find(Boolean).provider : p
+	const walletLink = new WalletLink({ appName: 'ENSLogin' });
+	const p          = ethers.getDefaultProvider(config.provider.network);
+	const provider   = (p instanceof ethers.providers.FallbackProvider) ? p.providerConfigs.find(Boolean).provider : p;
 
 	provider
 	.send('eth_chainId')
 	.then(chainid => {
-		resolve(walletLink.makeWeb3Provider(provider.connection.url, Number(chainid)))
+		const ethereum = walletLink.makeWeb3Provider(provider.connection.url, Number(chainid));
+		ethereum.disable = ethereum.close.bind(ethereum);
+		resolve(ethereum);
 	})
 	.catch(reject)
 })
